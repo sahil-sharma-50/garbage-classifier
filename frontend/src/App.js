52d42bddfa7e311eb1e987d211c325bacfe9984f
Sahil Sharma <sahilsharma21062000@gmail.com>
2025-11-15 17:21:41 +0100

    testing workflows

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,7 +56,7 @@ function App() {
 
       {/* Main Content */}
       <div className="content">
-        <h1 className="title">♻️ Waste Classifier using ML and FastAPI !!</h1>
+        <h1 className="title">♻️ Waste Classifier</h1>
         <p className="subtitle">
           Upload an image to quickly identify the waste type and recommended bin.
         </p>
